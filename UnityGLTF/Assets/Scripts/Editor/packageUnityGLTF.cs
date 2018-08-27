5ce4f37216c7b60ca9d404c19b513fe8ae96bdd1
Nisht Joseph <reachjlight@gmail.com>
2018-08-27 11:18:16 +1000

    Add packaging script

---
+++ UnityGLTF/Assets/Scripts/Editor/packageUnityGLTF.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class packageUnityGLTF : EditorWindow
+{
+    [MenuItem("GLTF/Make Package")]
+    public static void MakePackage()
+    {
+        string[] files = new string[]
+            {
+                "Assets/UnityGLTF",
+            };
+        AssetDatabase.ExportPackage(
+            files,
+            "UnityGLTF.unitypackage",
+            ExportPackageOptions.Recurse);
+    }
+}
+#endif
